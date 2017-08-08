b570cb4daef196124859678d96b4395c39cc5c3d
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2017-08-08 08:04:37 +0300

    Add dotenv data collector

---
+++ src/AppBundle/DataCollector/DotenvDataCollector.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+/**
+ * Collects environment variables loaded by Dotenv component.
+ *
+ * @author Oleg Voronkovich <oleg-voronkovich@yandex.ru>
+ */
+class DotenvDataCollector extends DataCollector
+{
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        $this->data['envs'] = array();
+
+        $loadedVars = array_filter(explode(',', getenv('SYMFONY_DOTENV_VARS')));
+
+        foreach ($loadedVars as $var) {
+            if (false !== getenv($var)) {
+                $this->data['envs'][$var] = getenv($var);
+            }
+        }
+    }
+
+    public function getEnvs()
+    {
+        return $this->data['envs'];
+    }
+
+    public function getName()
+    {
+        return 'app.dotenv_collector';
+    }
+}
